af52897f23deb0edde0e22b518eed79a43e37d95
Andrii Kravchuk <a.kravchuk.pn@ucu.edu.ua>
2024-04-08 12:20:51 +0300

    complete sorted insert

--- Linked Lists - Sorted Insert/exercise.py
+++ Linked Lists - Sorted Insert/exercise.py
@@ -1,27 +1,14 @@
-
-class Node(object):
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-    def __repr__(self):
-        return f'Node({self.data})'
-
-
 def sorted_insert(head, data):
     if head is not None:
+        new = Node(data)
+        if head.data >= data:
+            new.next = head
+            return new
         curr = head
-        while curr:
-            if isinstance(curr.next, Node):
-                if data >= curr.data and data <= curr.next.data:
-                    data = Node(data)
-                    data.next = curr.next
-                    curr.next = data
-                    return head
-            elif curr.next is None:
-                curr.next = data
-                return head
+        while curr.next and curr.next.data < data:
             curr = curr.next
+        new.next = curr.next
+        curr.next = new
+        return head
     else:
         return Node(data)
-
-print(sorted_insert(Node(1), 2))
